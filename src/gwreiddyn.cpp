0a9338b6c5ad23a89937b313aed6286f695359a3
Shams Kitz <dustractor@gmail.com>
2024-07-30 17:25:38 -0500

    one

---
+++ src/gwreiddyn.cpp
@@ -0,0 +1,87 @@
+#include "plugin.hpp"
+#include <cmath>
+
+struct Gwreiddyn : Module {
+	enum ParamId {
+		PARAMS_LEN
+	};
+	enum InputId {
+		ROOT_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		THIRTEEN_STEP_OUTPUT,
+		TWELVE_STEP_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHTS_LEN
+	};
+
+	Gwreiddyn() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configInput(ROOT_INPUT, "");
+		configOutput(THIRTEEN_STEP_OUTPUT, "");
+		configOutput(TWELVE_STEP_OUTPUT, "");
+	}
+
+	void process(const ProcessArgs& args) override {
+        double root_in = inputs[ROOT_INPUT].getVoltage();
+        int t = (int)trunc(0.1+(std::fmod(10.f + root_in, 1.f) * 12)) % 12;
+        float outv_13[12] = {
+            1.0,        // C    6
+            1.834,     // C#   7
+            2.667,     // D    8
+            3.5,       // D#   9
+            4.334,     // E    10
+            5.167,     // F    11
+            6.0,        // F#   0
+            6.834,     // G    1
+            7.667,     // G#   2
+            8.5,       // A    3
+            9.334,     // A#   4
+            10.167      // B    5
+        };
+        float outv_12[12] = {
+            0.84,
+            1.673,
+            2.507,
+            3.34,
+            4.173,
+            5.01,
+            5.84,
+            6.673,
+            7.507,
+            8.34,
+            9.173,
+            10.01
+        };
+
+        outputs[THIRTEEN_STEP_OUTPUT].setVoltage(outv_13[t],0);
+        outputs[TWELVE_STEP_OUTPUT].setVoltage(outv_12[t],0);
+
+        
+	}
+};
+
+
+struct GwreiddynWidget : ModuleWidget {
+	GwreiddynWidget(Gwreiddyn* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/gwreiddyn.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 12.7)), module, Gwreiddyn::ROOT_INPUT));
+
+		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 101.6)), module, Gwreiddyn::THIRTEEN_STEP_OUTPUT));
+		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 116.84)), module, Gwreiddyn::TWELVE_STEP_OUTPUT));
+
+	}
+};
+
+
+Model* modelGwreiddyn = createModel<Gwreiddyn, GwreiddynWidget>("gwreiddyn");
---
+++ src/pysgota.cpp
@@ -0,0 +1,116 @@
+#include "plugin.hpp"
+#include <cmath>
+
+
+struct Pysgota : Module {
+	enum ParamId {
+		START_VOLTAGE_KNOB_PARAM,
+		END_VOLTAGE_KNOB_PARAM,
+		SCALE_Y_KNOB_PARAM,
+		OFFSET_KNOB_PARAM,
+		SCALE_KNOB_PARAM,
+		MODULUS_KNOB_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		START_VOLTAGE_SOCKET_INPUT,
+		END_VOLTAGE_SOCKET_INPUT,
+		SCALE_Y_SOCKET_INPUT,
+		OFFSET_SOCKET_INPUT,
+		SCALE_SOCKET_INPUT,
+		MODULUS_SOCKET_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		POLYPHONIC_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHTS_LEN
+	};
+
+	Pysgota() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(START_VOLTAGE_KNOB_PARAM, -10.f, 10.f, -1.f, "Voltage Range Start");
+		configParam(END_VOLTAGE_KNOB_PARAM, -10.f, 10.f, 1.f, "Voltage Range End");
+		configParam(SCALE_Y_KNOB_PARAM, -10.f, 10.f, 1.f, "Scale Y");
+		configParam(OFFSET_KNOB_PARAM, -10.f, 10.f, 0.f, "Offset");
+		configParam(SCALE_KNOB_PARAM, -10.f, 10.f, 1.f, "Scale");
+		configParam(MODULUS_KNOB_PARAM, 0.f, 10.f, 1.f, "Modulus");
+		configInput(START_VOLTAGE_SOCKET_INPUT, "Voltage Range Start");
+		configInput(END_VOLTAGE_SOCKET_INPUT, "Voltage Range End");
+		configInput(SCALE_Y_SOCKET_INPUT, "Scale Y");
+		configInput(OFFSET_SOCKET_INPUT, "Offset");
+		configInput(SCALE_SOCKET_INPUT, "Scale");
+		configInput(MODULUS_SOCKET_INPUT, "Modulus");
+		configOutput(POLYPHONIC_OUTPUT, "Polyphonic");
+	}
+
+	void process(const ProcessArgs& args) override {
+        float vstart = params[START_VOLTAGE_KNOB_PARAM].getValue();
+        if (inputs[START_VOLTAGE_SOCKET_INPUT].isConnected()){
+            vstart = inputs[START_VOLTAGE_SOCKET_INPUT].getVoltage();
+        }
+        float vend = params[END_VOLTAGE_KNOB_PARAM].getValue();
+        if (inputs[END_VOLTAGE_SOCKET_INPUT].isConnected()){
+            vend = inputs[END_VOLTAGE_SOCKET_INPUT].getVoltage();
+        }
+        float scale_y = params[SCALE_Y_KNOB_PARAM].getValue();
+        if (inputs[SCALE_Y_SOCKET_INPUT].isConnected()){
+            scale_y = inputs[SCALE_Y_SOCKET_INPUT].getVoltage();
+        }
+        float offset = params[OFFSET_KNOB_PARAM].getValue();
+        if (inputs[OFFSET_SOCKET_INPUT].isConnected()){
+            offset = inputs[OFFSET_SOCKET_INPUT].getVoltage();
+        }
+        float scale = params[SCALE_KNOB_PARAM].getValue();
+        if (inputs[SCALE_SOCKET_INPUT].isConnected()){
+            scale = inputs[SCALE_SOCKET_INPUT].getVoltage();
+        }
+        float modulus = params[MODULUS_KNOB_PARAM].getValue();
+        if (inputs[MODULUS_SOCKET_INPUT].isConnected()){
+            modulus = inputs[MODULUS_SOCKET_INPUT].getVoltage();
+        }
+        float increment = (vend - vstart) / (PORT_MAX_CHANNELS - 1);
+        float range[PORT_MAX_CHANNELS];
+        if (!((scale == 0) || (modulus == 0))){
+            for (int i=0; i<PORT_MAX_CHANNELS; i++){
+                range[i] = fmod((offset + sin(scale_y * (vstart + increment * i))) * scale, modulus);
+            }
+        }
+        outputs[POLYPHONIC_OUTPUT].setChannels(PORT_MAX_CHANNELS);
+        outputs[POLYPHONIC_OUTPUT].writeVoltages(range);
+	}
+};
+
+
+struct PysgotaWidget : ModuleWidget {
+	PysgotaWidget(Pysgota* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/pysgota.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.16, 10.16)), module, Pysgota::START_VOLTAGE_KNOB_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.32, 10.16)), module, Pysgota::END_VOLTAGE_KNOB_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 35.56)), module, Pysgota::SCALE_Y_KNOB_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.16, 60.96)), module, Pysgota::OFFSET_KNOB_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.32, 60.96)), module, Pysgota::SCALE_KNOB_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 86.36)), module, Pysgota::MODULUS_KNOB_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16, 22.86)), module, Pysgota::START_VOLTAGE_SOCKET_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.32, 22.811)), module, Pysgota::END_VOLTAGE_SOCKET_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 48.26)), module, Pysgota::SCALE_Y_SOCKET_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16, 73.66)), module, Pysgota::OFFSET_SOCKET_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.32, 73.66)), module, Pysgota::SCALE_SOCKET_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 99.06)), module, Pysgota::MODULUS_SOCKET_INPUT));
+
+		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(15.24, 116.84)), module, Pysgota::POLYPHONIC_OUTPUT));
+	}
+};
+
+
+Model* modelPysgota = createModel<Pysgota, PysgotaWidget>("pysgota");
